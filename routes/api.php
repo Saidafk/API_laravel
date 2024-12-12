428e1d7c6beb3b86577ed07db14ae814102ee1e8
skheiar <skheiar@E201-17>
2024-12-12 12:08:24 +0100

    ajout réussi

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,5 @@ Route::get('/user', function (Request $request) {
 Route::get('/produits', [ProduitsController::class, "liste"]);
 
 Route::get('/produits/{id}', [ProduitsController::class, "detail"]);
+
+Route::post('/produits', [ProduitsController::class, "ajouter"]);
